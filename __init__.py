0cd01bee7e4a30ea5fdae566725af639923173a1
Mateusz Kolodziejczyk <mateusz.j.kolodziejczyk@gmail.com>
2020-08-28 10:32:30 +0200

    add Decimal and fix Errors

--- __init__.py
+++ __init__.py
@@ -1,34 +1,37 @@
 """ Draw with Percentage """
 
 from random import randint
+from decimal import Decimal
 
 
 def draw_with_percentage(percentages: list):
-    """
+    """Randomize an element according to the given percentages
 
-    :param percentages:
-    :return:
+    :param percentages: This is a list that will contain all items with the percentage indicated.
+    :return: Return the drawn element according to the set percentages.
     """
 
     if not len(percentages) > 0:
-        raise Exception("No item was specified")
+        raise ValueError("No item was specified")
 
     sum_percentages = 0
     for index, item in enumerate(percentages):
-        if isinstance(item[0], str) and isinstance(item[1], float) or isinstance(item[1], int):
+        if isinstance(item[0], str) and isinstance(item[1], str):
+            item[1] = Decimal(item[1])
             sum_percentages += item[1]
         else:
-            raise Exception("Bad given items")
+            raise ValueError("Bad given items")
 
         if index == 0:
-            item[1] = [0, item[1]]
+            item[1] = [Decimal(0), item[1]]
         else:
-            start = percentages[index - 1][1][1] + 0.001 if percentages[index - 1][1][1] != 0 else 0
+            start = percentages[index - 1][1][1] + Decimal("0.001") \
+                if percentages[index - 1][1][1] != 0 else Decimal("0")
             end = percentages[index - 1][1][1] + item[1]
             item[1] = [start, end]
 
     if sum_percentages != 100:
-        raise Exception("The sum of all the percentages is not 100")
+        raise ValueError("The sum of all the percentages is not 100")
 
     random_number = randint(1000, 100000) / 1000
 
@@ -39,10 +42,10 @@ def draw_with_percentage(percentages: list):
 
 if __name__ == "__main__":
     obj = [
-        ["milk", 24.5],
-        ["apple", 24.91],
-        ["bread", 39.09],
-        ["banana", 11.5]
+        ["milk", "23.401"],
+        ["apple", "25.91"],
+        ["bread", "39.09"],
+        ["banana", "11.599"]
     ]
 
     print(draw_with_percentage(obj))
