4be7b16421906af68eeb1e112b0225c5c6df9208
Mateusz Kolodziejczyk <mateusz.j.kolodziejczyk@gmail.com>
2020-08-28 12:12:02 +0200

    update and add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='draw with percentages',
+      version='0.0.1',
+      description='Python Library to draw with percentage',
+      author='Mateusz Kołodziejczyk',
+      author_email='mateusz.j.kolodziejczyk@gmail.com',
+      license='MIT',
+      packages=find_packages()
+      )
